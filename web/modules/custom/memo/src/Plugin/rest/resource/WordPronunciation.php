112122759ae01be057baa69ba3ec382841c83d65
yvandouenel <y.douenel@coopernet.fr>
2020-08-20 12:42:56 +0200

    WordPronunciation.php added.

---
+++ web/modules/custom/memo/src/Plugin/rest/resource/WordPronunciation.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\memo\Plugin\rest\resource;
+
+use Drupal\rest\Plugin\ResourceBase;
+//use Symfony\Component\HttpFoundation\JsonResponse;
+use Drupal\rest\ResourceResponse;
+
+/**
+ * Provides a Demo Resource
+ *
+ * @RestResource(
+ *   id = "pronunciation",
+ *   label = @Translation("Cartes - Pronunciation"),
+ *   uri_paths = {
+ *     "canonical" = "/memo/pronunciation/{word}"
+ *   }
+ * )
+ */
+class WordPronunciation extends ResourceBase {
+  /**
+   * Responds to entity GET requests.
+   * @return \Drupal\rest\ResourceResponse
+   */
+  public function get($word = "") {
+    $key = "k5HRNv5msL";
+    $url = "http://voicecup.com/api?" .
+      "q=" . $word . "&" .
+      "key=" . $key .
+      "&l=en&from=10&size=15&length_min=15&length_max=50&duration_min=5&duration_max=25&format=jsonp";
+    $data = file_get_contents($url);
+    
+    return new ResourceResponse($data);
+    //return new JsonResponse($data);
+  }
+}
